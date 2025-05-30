89992b1beb36ff27867f72e92db1ef94372eca9f
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-05-30 17:48:18 +0100

    modified email is verified middleware

--- app/Http/Middleware/EnsureEmailIsVerified.php
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,6 +16,21 @@ class EnsureEmailIsVerified
      */
     public function handle(Request $request, Closure $next): Response
     {
+       if (! $request->user() ||
+            ($request->user() instanceof MustVerifyEmail &&
+            ! $request->user()->hasVerifiedEmail())) {
+
+            // return response()->json(['message' => 'Your email address is not verified.'], 409);
+
+            $response['success'] = false;
+            $response['message'] = 'Your email address is not verified.';
+            $statusCode = 409;
+
+            // return response()->json($response, $statusCode);
+            return redirect()->away(config('app.frontend_verify_email_error_url') . '?message=email_not_verified&status=409');
+
+        }
+
         return $next($request);
     }
 }
